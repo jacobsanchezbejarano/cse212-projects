b0922b73e42956a78396b748f39146b2c9a54a43
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-03-13 23:41:30 -0400

    W03 :: Learn 2

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -6,9 +6,13 @@ public class Translator
         englishToGerman.AddWord("House", "Haus");
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
+        englishToGerman.AddWord("Cat", "Katze");
+        englishToGerman.AddWord("Apple", "Apfel");
         Console.WriteLine(englishToGerman.Translate("Car")); // Auto
         Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
         Console.WriteLine(englishToGerman.Translate("Train")); // ???
+        Console.WriteLine(englishToGerman.Translate("Cat")); // Katze
+        Console.WriteLine(englishToGerman.Translate("Apple")); // Apfel
     }
 
     private Dictionary<string, string> _words = new();
@@ -24,7 +28,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words.Add(fromWord, toWord);
     }
 
     /// <summary>
@@ -34,7 +38,9 @@ public class Translator
     /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        if (_words.ContainsKey(fromWord))
+            return _words[fromWord];
+        else
+            return "???";
     }
 }
