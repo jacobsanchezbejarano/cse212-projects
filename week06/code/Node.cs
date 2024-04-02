a2085ff9fe25f04a69b8dd98a77b7c86087fb1ca
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-04-01 23:42:11 -0400

    W06 Code :: Trees :: 1-4

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,36 +1,85 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data)
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
+        else
+        {
+            return;
+        }
     }
 
-    public bool Contains(int value) {
-        // TODO Start Problem 2
+    public bool Contains(int value)
+    {
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else if (value > Data)
+        {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
         return false;
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        var left = 1;
+        var right = 1;
+        var Left_aux = Left;
+        var Right_aux = Right;
+
+        while (Left_aux != null)
+        {
+            Left_aux = Left_aux.Left;
+            left += 1;
+        }
+        while (Right_aux != null)
+        {
+            Right_aux = Right_aux.Right;
+            right += 1;
+        }
+
+        if (left > right)
+            return left;
+        else if (right > left)
+            return right;
+        else
+            return left;
     }
 }
